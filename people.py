eb09b0f933f6a7697d5ca262bbc8a96779c6b258
Danish-Belal <danishexplore019@gmail.com>
2023-06-03 17:31:15 +0530

    🚧 Endpoints get , create , update , delete

--- people.py
+++ people.py
@@ -1,9 +1,58 @@
 # people.py
 
 from datetime import datetime
+from flask import abort
 
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+
+def create(person):
+    lname = person.get("lname")
+    fname = person.get("fname", "")
+    
+    if lname and lname not in PEOPLE:
+        PEOPLE[lname]={
+            "lname": lname,
+            "fname": fname,
+            "timestamp": get_timestamp(),
+        }
+        return PEOPLE[lname] , 201
+    else:
+        abort(
+            406,
+            f"Person with lastname {lname} already axist"
+        )
+
+def read_one(lname):
+    if lname in PEOPLE:
+        return PEOPLE[lname]
+    else:
+        abort(
+            404, f"Person with last name {lname} not found"
+        )
+
+def update(lname, person):
+    if lname in PEOPLE:
+        PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
+        PEOPLE[lname]["timestamp"] = get_timestamp()
+        return PEOPLE[lname]
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
+        )
+
+def delete(lname):
+    if lname in PEOPLE:
+        del PEOPLE[lname]
+        return make_response(
+            f"{lname} successfully deleted", 200
+        )
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
+        )
 
 PEOPLE = {
     "Fairy": {
